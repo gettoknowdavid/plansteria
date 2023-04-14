74eca37943e26e61a9436aafbacfef3dc806b085
David Michael II <knowdavidmichael@gmail.com>
2023-04-14 21:44:45 +0100

    fix: avatar url null safety

--- lib/ui/widgets/event_details/creator_section.dart
+++ lib/ui/widgets/event_details/creator_section.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:phosphor_flutter/phosphor_flutter.dart';
 import 'package:plansteria/models/user.dart';
 import 'package:plansteria/ui/views/event_details/event_details_viewmodel.dart';
 import 'package:stacked/stacked.dart';
@@ -20,29 +21,47 @@ class CreatorSection extends ViewModelWidget<EventDetailsViewModel> {
         }
 
         final creatorName = snapshot.data?.name;
+        final avatar = snapshot.data?.avatar;
+
         return Row(
           children: [
             Container(
               height: 54.r,
               width: 54.r,
               decoration: BoxDecoration(
-                image: const DecorationImage(
-                  fit: BoxFit.cover,
-                  image: AssetImage('assets/tec-conference.jpg'),
-                ),
-                borderRadius: const BorderRadius.all(
-                  Radius.circular(16),
-                ).r,
+                color: theme.primaryColor.withOpacity(0.2),
+                borderRadius: const BorderRadius.all(Radius.circular(16)).r,
+                image: avatar == null
+                    ? null
+                    : DecorationImage(
+                        image: NetworkImage(avatar),
+                        fit: BoxFit.cover,
+                      ),
+              ),
+              child: avatar == null ? const Icon(PhosphorIcons.user) : null,
+            ),
+            12.horizontalSpace,
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('CREATED BY', style: textTheme.bodySmall),
+                  Text('$creatorName', style: textTheme.bodyLarge)
+                ],
               ),
             ),
             12.horizontalSpace,
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text('CREATED BY', style: textTheme.bodySmall),
-                Text('$creatorName', style: textTheme.bodyLarge)
-              ],
+            GestureDetector(
+              onTap: () {},
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Icon(PhosphorIcons.share),
+                  4.verticalSpace,
+                  Text('Share', style: textTheme.bodySmall),
+                ],
+              ),
             ),
           ],
         );
